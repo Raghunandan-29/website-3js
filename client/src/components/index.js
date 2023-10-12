9a583a1cecb5641d0f0a5d0fb35f7c294d818a94
Raghunandan NC <nandan@RaghunandansMBP.mshome.net>
2023-10-12 23:19:09 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tabs from "./Tabs";
+export { CustomButton,
+         AIPicker,
+         Tabs,
+         ColorPicker,
+         FilePicker };
